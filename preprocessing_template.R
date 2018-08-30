4be7d2dd55cf1124f3c0eab82d231d7c1353ac99
Jason Yingling <jason03yingling@gmail.com>
2018-08-30 18:37:51 -0500

    Update preprocessing_template.R

--- preprocessing_template.R
+++ preprocessing_template.R
@@ -4,7 +4,6 @@
 dataset = read.csv('Data.csv')
 
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
